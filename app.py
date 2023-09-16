d2d29f05db8749633f87c093e9ea5887421f9d23
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2023-09-17 00:01:09 +0200

    Persist variants and votes in a database
    * replace the global in-memory dictionary by a database
    * remove the auto-generation of test data
    * also add an endpoint for exporting the votes as json dictionary
    * also add an endpoint for creating new coffee variants, protected by a
      configurable admin token
    * if no such token is configured, it will be auto-generated and printed
      during startup

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-import sys
-from collections import defaultdict
-from dataclasses import dataclass
 from enum import IntEnum
-from typing import Dict, List
+from typing import Dict, List, Tuple
+from uuid import uuid4
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, current_app, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError, NoResultFound
+
+app = Flask("coffee")
+app.config.from_pyfile("coffee.cfg")
+app.template_folder = "templates"
+sa = SQLAlchemy(app)
+
+if "ADMIN_TOKEN" not in app.config:
+    token = str(uuid4())
+    print(f"admin token: {token}")
+    app.config["ADMIN_TOKEN"] = token
 
 
 class VoteEnum(IntEnum):
@@ -12,25 +22,56 @@ class VoteEnum(IntEnum):
     Upvote = 1
 
 
-@dataclass
-class Vote:
-    name: str
-    vote: VoteEnum
+class CoffeeVariant(sa.Model):
+    __tablename__ = "coffee_variants"
+
+    id: sa.Mapped[int] = sa.mapped_column(primary_key=True)
+    name: sa.Mapped[str] = sa.mapped_column(sa.String(), unique=True)
+    votes: sa.Mapped[List["Vote"]] = sa.relationship(back_populates="variant")
 
 
-app = Flask("coffee")
-app.config.from_pyfile("coffee.cfg")
-app.template_folder = "templates"
-votes = defaultdict(list)
+class Vote(sa.Model):
+    __tablename__ = "votes"
+
+    id: sa.Mapped[int] = sa.mapped_column(primary_key=True)
+    variant_id: sa.Mapped[int] = sa.mapped_column(sa.ForeignKey("coffee_variants.id"))
+    variant: sa.Mapped[CoffeeVariant] = sa.relationship(back_populates="votes")
+    name: sa.Mapped[str] = sa.mapped_column(sa.String())
+    vote: sa.Mapped[VoteEnum] = sa.mapped_column(sa.Integer)
 
 
-def _add_vote(coffee_variant: str, name: str, vote: VoteEnum) -> bool:
-    for existing_vote in votes[coffee_variant]:
-        if existing_vote.name.lower() == name.lower():
-            return False
+def _get_votes() -> Dict[str, List[Vote]]:
+    votes = sa.session.execute(sa.select(Vote)).scalars()
+    variants = sa.session.execute(sa.select(CoffeeVariant)).scalars()
+    votes_dict = {v.name: [] for v in variants}
+    for vote in votes:
+        votes_dict[vote.variant.name].append(vote)
 
-    votes[coffee_variant].append(Vote(name=name, vote=vote))
-    return True
+    return votes_dict
+
+
+def _add_vote(coffee_variant: str, name: str, vote: VoteEnum) -> Tuple[bool, str]:
+    try:
+        votes = _get_votes()
+        for existing_vote in votes[coffee_variant]:
+            if existing_vote.name.lower() == name.lower():
+                sa.session.rollback()
+                return False, "no double votes!"
+
+        variant = sa.session.execute(
+            sa.select(CoffeeVariant).filter(CoffeeVariant.name == coffee_variant)
+        ).scalar_one()
+        v = Vote(variant=variant, name=name, vote=vote)
+        sa.session.add(v)
+        sa.session.commit()
+        return True, "thanks for voting!"
+
+    except NoResultFound:
+        return False, "coffee variant not found!"
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return False, "unexpected error"
 
 
 @app.template_filter()
@@ -54,35 +95,78 @@ def display_vote(vote: VoteEnum) -> str:
 
 @app.route("/")
 def show_overview():
+    votes = _get_votes()
     return render_template("overview.jinja", votes=votes)
 
 
-@app.route("/vote", methods=["POST"])
-def vote():
-    try:
-        variant = request.values["variant"]
-        vote = request.values["vote"]
-        name = request.values["name"]
+@app.route("/export")
+def export():
+    votes = {
+        variant: {
+            vote.name: "+" if vote.vote == VoteEnum.Upvote else "-" for vote in votes
+        }
+        for variant, votes in _get_votes().items()
+    }
+    return jsonify(votes)
 
-        if _add_vote(variant, name, VoteEnum(int(vote))):
-            return jsonify({"message": "thanks for voting!"})
-        else:
-            return jsonify({"message": "no double votes!"}), 403
+
+@app.route("/variants", methods=["POST"])
+def add_coffee_variant():
+    try:
+        values = request.values.to_dict()
+        token = request.values.get("token", None)
+        if token is None:
+            return jsonify({"message": "token required!"}), 401
+
+        elif token != current_app.config["ADMIN_TOKEN"]:
+            return jsonify({"message": "unrecognized token!"}), 403
+
+        name = values["name"]
+        variant = CoffeeVariant(name=name)
+        sa.session.add(variant)
+        sa.session.commit()
+
+        return jsonify({"message": "added variant!"})
 
     except KeyError as e:
-        return jsonify({"message": f"could not find the value for {e}"}), 400
+        app.logger.error(e)
+        return jsonify({"message": f"missing the required value for {e}"}), 400
+
+    except IntegrityError as e:
+        app.logger.error(e)
+        return jsonify({"message": "variant already exists!"}), 403
 
     except Exception as e:
-        print(e, file=sys.stderr)
+        app.logger.error(e)
         return jsonify({"message": e}), 400
 
 
-def _add_test_data(app: Flask, votes: Dict[str, List[Vote]]) -> None:
-    if app.config.get("TESTING", False):
-        _add_vote("Corsini", "Max", VoteEnum.Upvote)
-        _add_vote("Indiana", "Max", VoteEnum.Downvote)
+@app.route("/vote", methods=["POST"])
+def add_vote():
+    try:
+        values = request.values.to_dict()
+        variant = values["variant"]
+        vote = VoteEnum(int(values["vote"]))
+        name = values["name"]
+
+        success, message = _add_vote(variant, name, vote)
+        return jsonify({"message": message}), 200 if success else 403
+
+    except KeyError as e:
+        app.logger.error(e)
+        return jsonify({"message": f"missing the required value for {e}"}), 400
+
+    except ValueError as e:
+        app.logger.error(e)
+        return jsonify({"message": "unexpected value for the vote"}), 400
+
+    except Exception as e:
+        app.logger.error(e)
+        return jsonify({"message": e}), 400
 
 
-_add_test_data(app, votes)
+with app.app_context():
+    sa.create_all()
+
 if __name__ == "__main__":
     app.run()
